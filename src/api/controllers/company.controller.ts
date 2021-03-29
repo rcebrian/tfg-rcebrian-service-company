7adc2de455526309c832e62447a83bceb6ed45f5
rcebrian <rodrigocebriang@gmail.com>
2021-03-29 22:13:22 +0200

    REFACTOR : se cambia el response al crear una company

--- src/api/controllers/company.controller.ts
+++ src/api/controllers/company.controller.ts
@@ -7,17 +7,9 @@ export const create = (req: Request, res: Response) => {
   Company.create({
     name: req.body.name,
     description: req.body.description,
-  }).then((company) => {
-    const data = {
-      company,
-      _links: {
-        self: {
-          href: `${req.protocol}://${req.get('host')}/api/company/${company.id}`,
-        },
-      },
-    };
+  }).then((data) => {
     res.status(httpStatus.CREATED)
-      .json(data);
+      .json({ data });
   }).catch((err: ValidationError) => {
     res.status(httpStatus.BAD_REQUEST).json({ errors: err.message.split('\n') });
   }).catch((err) => {
