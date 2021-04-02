1f97733b16a51efb48dd0c33d02ca86fc31d63bc
rcebrian <rodrigocebriang@gmail.com>
2021-04-02 15:07:53 +0200

    ADD : operacion update

--- src/api/routes/company.route.ts
+++ src/api/routes/company.route.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import {
-  create, findAll, findById, remove,
+  create, findAll, findById, remove, update,
 } from '../controllers/company.controller';
 
 const router = Router();
 
-router.route('/').post(create);
 router.route('/').get(findAll);
+router.route('/').post(create);
+
 router.route('/:id([0-9]+)').get(findById);
+router.route('/:id([0-9]+)').put(update);
 router.route('/:id([0-9]+)').delete(remove);
 
 export { router as companyRoutes };
