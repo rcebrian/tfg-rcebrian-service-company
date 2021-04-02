84f4a63b0d2186bc6fdc2fdd7d8c00c7f6890313
rcebrian <rodrigocebriang@gmail.com>
2021-04-02 20:24:35 +0200

    DOCS : doc del metodo

--- src/api/controllers/groups.controller.ts
+++ src/api/controllers/groups.controller.ts
@@ -2,6 +2,11 @@ import httpStatus from 'http-status';
 import { Request, Response } from 'express';
 import { Group } from '../repository/mysql/mysql.repository';
 
+/**
+ * Get all groups from a company
+ * @param req GET method
+ * @param res List of groups inside a company
+ */
 export const findAll = (req: Request, res: Response) => {
   const { companyId } = req.params;
   Group.findAll({ where: { companyId } }).then((data) => {
