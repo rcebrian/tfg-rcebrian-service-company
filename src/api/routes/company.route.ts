b141af5c049d85d987f94240741c6087f531b81e
rcebrian <rodrigocebriang@gmail.com>
2021-04-02 13:57:05 +0200

    ADD : implementation

--- src/api/routes/company.route.ts
+++ src/api/routes/company.route.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { create, findAll, findById } from '../controllers/company.controller';
+import {
+  create, findAll, findById, remove,
+} from '../controllers/company.controller';
 
 const router = Router();
 
 router.route('/').post(create);
 router.route('/').get(findAll);
 router.route('/:id([0-9]+)').get(findById);
+router.route('/:id([0-9]+)').delete(remove);
 
 export { router as companyRoutes };
