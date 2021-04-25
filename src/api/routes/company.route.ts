7c1ac3ea20b9db133bfbbf993e00b124acee7d0b
rcebrian <rodrigocebriang@gmail.com>
2021-04-25 14:11:22 +0200

    :heavy_plus_sign: @rcebrian/tfg-rcebrian-common & update domain

--- src/api/routes/company.route.ts
+++ src/api/routes/company.route.ts
@@ -1,14 +1,19 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import httpStatus from 'http-status';
+import { roleAdmin, validatorHandler } from '@rcebrian/tfg-rcebrian-common';
 import {
   create, findAll, findById, remove, update,
 } from '../controllers/company.controller';
-import { validator } from '../middlewares';
+
 import { createNew } from '../validations';
 
 const router = Router();
 
+router.route('/admin').get(roleAdmin, (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({ message: 'Access granted' });
+});
 router.route('/').get(findAll);
-router.route('/').post(createNew(), validator, create);
+router.route('/').post(createNew(), validatorHandler, create);
 
 router.route('/:id([0-9]+)').get(findById);
 router.route('/:id([0-9]+)').put(update);
