75f3095fadc7e3481ef881cbe2e0765f4e79ac23
rcebrian <rodrigocebriang@gmail.com>
2021-04-25 16:38:10 +0200

    :green_heart: import from common

--- src/api/routes/groups.route.ts
+++ src/api/routes/groups.route.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
+import { validatorHandler } from '@rcebrian/tfg-rcebrian-common';
 import {
   create, findAll, findById, remove, update,
 } from '../controllers/groups.controller';
 import { createNew } from '../validations';
-import { validator } from '../middlewares';
 
 const router = Router();
 
 router.route('/:companyId([0-9]+)/groups/').get(findAll);
-router.route('/:companyId([0-9]+)/groups/').post(createNew(), validator, create);
+router.route('/:companyId([0-9]+)/groups/').post(createNew(), validatorHandler, create);
 
 router.route('/:companyId([0-9]+)/groups/:groupId([0-9]+)').get(findById);
 router.route('/:companyId([0-9]+)/groups/:groupId([0-9]+)').put(update);
