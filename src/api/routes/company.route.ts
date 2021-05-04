534410ff9d73f6ea74b0448dc80f1e2bc461d360
rcebrian <rodrigocebriang@gmail.com>
2021-05-05 00:29:46 +0200

    :lock: routes middleware

--- src/api/routes/company.route.ts
+++ src/api/routes/company.route.ts
@@ -12,9 +12,9 @@ router.route('/').get(roleAdmin, findAll);
 router.route('/tree').get(roleAdmin, findAllCompanyTree);
 router.route('/').post(roleAdmin, createNew(), validatorHandler, create);
 
-router.route('/:companyId([0-9]+)/tree').get(roleAdmin, roleCompany, findByIdCompanyTree);
+router.route('/:companyId([0-9]+)/tree').get(roleAdmin, findByIdCompanyTree);
 router.route('/:id([0-9]+)').get(roleAdmin, roleCompany, findById);
 router.route('/:id([0-9]+)').put(roleAdmin, createNew(), validatorHandler, update);
-router.route('/:id([0-9]+)').delete(roleAdmin, createNew(), validatorHandler, remove);
+router.route('/:id([0-9]+)').delete(roleAdmin, remove);
 
 export { router as companyRoutes };
