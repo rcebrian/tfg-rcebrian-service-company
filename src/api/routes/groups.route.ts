b81e90eb13e768a3aa1bfee79fee5124ef2b2755
rcebrian <rodrigocebriang@gmail.com>
2021-05-15 18:31:23 +0200

    groups routes with auth

--- src/api/routes/groups.route.ts
+++ src/api/routes/groups.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { validatorHandler, auth } from '@rcebrian/tfg-rcebrian-common';
+import { validatorHandler, auth, roleAdmin } from '@rcebrian/tfg-rcebrian-common';
 import {
   create, findAll, groupTree, remove, update, addUserToGroup,
 } from '../controllers/groups.controller';
@@ -8,12 +8,12 @@ import { createNew, addUserToGroupValidation } from '../validations';
 const router = Router();
 
 router.route('/').get(auth, findAll);
-router.route('/').post(createNew(), validatorHandler, create);
+router.route('/').post(auth, roleAdmin, createNew(), validatorHandler, create); // ok
 
-router.route('/:groupId([0-9]+)').get(groupTree);
-router.route('/:groupId([0-9]+)').put(update);
-router.route('/:groupId([0-9]+)').delete(remove);
+router.route('/:groupId([0-9]+)').get(auth, groupTree);
+router.route('/:groupId([0-9]+)').put(auth, update);
+router.route('/:groupId([0-9]+)').delete(auth, remove);
 
-router.route('/:groupId([0-9]+)/users').put(addUserToGroupValidation(), validatorHandler, addUserToGroup);
+router.route('/:groupId([0-9]+)/users').put(auth, addUserToGroupValidation(), validatorHandler, addUserToGroup);
 
 export { router as groupsRoutes };
