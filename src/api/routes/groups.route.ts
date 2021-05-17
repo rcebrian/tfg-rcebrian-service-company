a7ffbff0dddea9a46b60360f67e345e01a541121
rcebrian <rodrigocebriang@gmail.com>
2021-05-17 22:06:24 +0200

    :sparkles: new group validation

--- src/api/routes/groups.route.ts
+++ src/api/routes/groups.route.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import {
-  validatorHandler, auth, roleAdmin, roleMonitor,
+  validatorHandler, auth, roleAdmin,
 } from '@rcebrian/tfg-rcebrian-common';
 import {
   create, findAll, groupTree, remove, update, addUserToGroup,
 } from '../controllers/groups.controller';
-import { createNew, addUserToGroupValidation } from '../validations';
+import { addUserToGroupValidation, createNewGroup } from '../validations';
 
 const router = Router();
 
-router.route('/').get(auth, findAll); // deprecated
-router.route('/').post(auth, roleAdmin, createNew(), validatorHandler, create); // ok
+router.route('/').get(auth, findAll); // ok
+router.route('/').post(auth, roleAdmin, createNewGroup(), validatorHandler, create); // ok
 
-router.route('/:groupId([0-9]+)').get(auth, (roleAdmin || roleMonitor), groupTree); // ok
+router.route('/:groupId([0-9]+)').get(auth, groupTree); // ok
 router.route('/:groupId([0-9]+)').put(auth, update); // deprecated
 router.route('/:groupId([0-9]+)').delete(auth, remove); // deprecated
 
